eda9457ef214510020a6bdec81c55662a63d0ab7
Zubair Khalid <msds20010@itu.edu.pk>
2021-06-28 01:00:23 +0500

    fixed bugs

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
-import Rating from '@material-ui/lab/Rating';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import axios from 'axios';
 import moment from 'moment';
@@ -13,7 +12,7 @@ export default function Details() {
   let { id } = useParams();
   
   const [document, setDocument] = useState({});
-  const [selectedTab, setTab] = useState(0);
+
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -40,7 +39,7 @@ export default function Details() {
   if (!isLoading && document) {
     let image_src = "/images/categories/"+document?.category+".jpg"
     let author = document?.author
-    if(author == "" || author == null){
+    if(author === "" || author == null){
       author = "Unknown"
     }
 
@@ -93,7 +92,7 @@ export default function Details() {
 
                   {link.length > 20 &&
                     <span className="read-externel">
-                      <a href={link} target="_blank">Read Original Content <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right-square-fill icon" viewBox="0 0 16 16">
+                      <a href={link} rel="noreferrer" target="_blank">Read Original Content <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right-square-fill icon" viewBox="0 0 16 16">
                         <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z"/>
                         </svg>
                       </a>
--- src/pages/Details/ListComponent.js
+++ src/pages/Details/ListComponent.js
@@ -75,7 +75,7 @@ export default function List(props){
                    {loc}
                 </span>
               }
-              {props.title != "Sentiments" &&
+              {props.title !== "Sentiments" &&
                 <span>{loc}</span>
               }
              
